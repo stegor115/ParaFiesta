d3eb37b78bb9a472e3ddab9bb681fe8278cb8b49
Stephen Gordon <stegor115@gmail.com>
2018-07-07 02:15:19 -0400

    Initial commit

---
+++ Source/ParaFiesta/ParaFiestaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ParaFiestaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARAFIESTA_API AParaFiestaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
