1c2758b71f46d00f94eb0cd2b77c152c789a9b39
Stephen Gordon <stegor115@gmail.com>
2018-07-07 04:00:04 -0400

    Moved Folders

--- Source/ParaFiesta/PlayerAvatar.cpp
+++ Source/ParaFiesta/PlayerAvatar.cpp
@@ -1,21 +1,47 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PlayerAvatar.h"
-
+#include "Engine/World.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 APlayerAvatar::APlayerAvatar()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//Character Movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->JumpZVelocity = 600.f;
+	GetCharacterMovement()->AirControl = 0.2f;
+
+	//Camera Boom (Big stick that keeps camera away)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f; //Length of the "stick"
+	CameraBoom->bUsePawnControlRotation = true; //Allows the mouse to rotate the stick relative to the player
+
+												//Camera
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); //Attach camera to the end of the stick of the boom
+	Camera->bUsePawnControlRotation = false; //Prevents camera itself from rotating.
+
+											 //Cube
+	SpawnerCube = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SpawnerCube"));
+	SpawnerCube->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
 void APlayerAvatar::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -29,6 +55,79 @@ void APlayerAvatar::Tick(float DeltaTime)
 void APlayerAvatar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	//Actions
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	//Camera Handling
+	PlayerInputComponent->BindAction("ZoomOut", IE_Pressed, this, &APlayerAvatar::ZoomOut);
+	PlayerInputComponent->BindAction("ZoomIn", IE_Pressed, this, &APlayerAvatar::ZoomIn);
+	//Basic Movement
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerAvatar::MoveForward);
+	PlayerInputComponent->BindAxis("MoveBackward", this, &APlayerAvatar::MoveBackward);
+	PlayerInputComponent->BindAxis("MoveLeft", this, &APlayerAvatar::MoveLeft);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerAvatar::MoveRight);
+	//Mouse Turning
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
+//Camera Handling--------------------------------------------------------------
+void APlayerAvatar::ZoomOut()
+{
+	if (CameraBoom->TargetArmLength < MAX_CAMERA_DISTANCE) {
+		CameraBoom->TargetArmLength += 10.0f;
+	} //end if
+}
+void APlayerAvatar::ZoomIn()
+{
+	if (CameraBoom->TargetArmLength > MIN_CAMERA_DISTANCE) {
+		CameraBoom->TargetArmLength -= 10.0f;
+	} //end if
+}
+//Movement---------------------------------------------------------------------
+void APlayerAvatar::MoveForward(float value)
+{
+	if (Controller && value) {
+		//Finds which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		//Get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, value);
+	} //end if
+}
+
+void APlayerAvatar::MoveBackward(float value)
+{
+	if (Controller && value) {
+		//Finds which way is backward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		//Gets "forward" vector (negative direction)
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, value);
+	} //end if
+}
+
+void APlayerAvatar::MoveLeft(float value)
+{
+	if (Controller && value) {
+		//Finds which way is left
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		//Gets "right" vector (negative direction)
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, value);
+	} //end if
+}
+
+void APlayerAvatar::MoveRight(float value)
+{
+	if (Controller && value) {
+		//Finds which way is right
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		//Gets right vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, value);
+	} //end if
+}
--- Source/ParaFiesta/PlayerAvatar.h
+++ Source/ParaFiesta/PlayerAvatar.h
@@ -10,6 +10,17 @@ UCLASS()
 class PARAFIESTA_API APlayerAvatar : public ACharacter
 {
 	GENERATED_BODY()
+	//Camera boom
+	UPROPERTY(VisibleAnywhere, BluePrintReadWrite, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	//Camera
+	UPROPERTY(VisibleAnywhere, BluePrintReadWrite, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* Camera;
+
+	//Cube Spawner
+	UPROPERTY(VisibleAnywhere, BluePrintReadWrite, Category = Cube, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* SpawnerCube;
 
 public:
 	// Sets default values for this character's properties
@@ -19,13 +30,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
-	
+	//Camera Handling
+	void ZoomOut();
+	void ZoomIn();
+	//Movement
+	void MoveForward(float value);
+	void MoveBackward(float value);
+	void MoveLeft(float value);
+	void MoveRight(float value);
+
+private:
+	int MAX_CAMERA_DISTANCE = 800;
+	int MIN_CAMERA_DISTANCE = 200;
 };
